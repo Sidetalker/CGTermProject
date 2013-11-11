ed64d1f0c68f1fcf8a6b2f31c7bca26dbdaa75a1
Kevin Sullivan <sideslapd@gmail.com>
2013-11-11 18:08:53 -0500

    Initial Commit

---
+++ CGTermProject/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CGTermProject
+//
+//  Created by Kevin Sullivan on 11/11/13.
+//  Copyright (c) 2013 com.sideapps. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
