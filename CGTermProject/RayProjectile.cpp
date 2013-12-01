1f32192da5430d2838513dcaaa90645181aca10b
okawaw <okawaw@gmail.com>
2013-12-01 15:17:09 -0500

    Prevent double hit on ray projectile

--- CGTermProject/RayProjectile.cpp
+++ CGTermProject/RayProjectile.cpp
@@ -29,35 +29,39 @@ void RayProjectile::checkCollisions( BaseTarget* targets[], uint numTargets )
 	// iterate through all current targets
 	for ( uint i = 0; i < numTargets; ++i )
 	{
-		switch ( targets[ i ]->getType() )
+		// prevent double hit
+		if ( targets[ i ]->getStatus() == TargetStatus::ACTIVE )
 		{
-			case TargetTypes::BULLSEYE: // TODO: can be cleaned up?
+			switch ( targets[ i ]->getType() )
 			{
-				Target* t = ( Target* ) targets[ i ];
-
-				// check intersection with front circle face
-				Vector intersectionNear = t->getNearPlane().lineIntersect( Line( m_center, m_velocity ) );
-
-				if ( ( intersectionNear - t->getNearPlane().getPoint() ).squareMagnitude() <= pow( t->getRadius(), 2 ) )
+				case TargetTypes::BULLSEYE: // TODO: can be cleaned up?
 				{
-					targets[ i ]->setStatus( TargetStatus::HIT );
+					Target* t = ( Target* ) targets[ i ];
+
+					// check intersection with front circle face
+					Vector intersectionNear = t->getNearPlane().lineIntersect( Line( m_center, m_velocity ) );
+
+					if ( ( intersectionNear - t->getNearPlane().getPoint() ).squareMagnitude() <= pow( t->getRadius(), 2 ) )
+					{
+						targets[ i ]->setStatus( TargetStatus::HIT );
+						break;
+					}
+
+					// check intersection with back circle face
+					Vector intersectionFar = t->getFarPlane().lineIntersect( Line( m_center, m_velocity ) );
+
+					if ( ( intersectionFar - t->getFarPlane().getPoint() ).squareMagnitude() <= pow( t->getRadius(), 2 ) )
+					{
+						targets[ i ]->setStatus( TargetStatus::HIT );
+					}
+
 					break;
 				}
-
-				// check intersection with back circle face
-				Vector intersectionFar = t->getFarPlane().lineIntersect( Line( m_center, m_velocity ) );
-
-				if ( ( intersectionFar - t->getFarPlane().getPoint() ).squareMagnitude() <= pow( t->getRadius(), 2 ) )
+				default:
 				{
-					targets[ i ]->setStatus( TargetStatus::HIT );
+					// invalid target type
+					break;
 				}
-
-				break;
-			}
-			default:
-			{
-				// invalid target type
-				break;
 			}
 		}
 	}
